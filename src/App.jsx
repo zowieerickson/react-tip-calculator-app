14271644994138b1b71243879a483a46bc8c8db3
Zowie Erickson <zowie.erickson@gmail.com>
2023-06-24 08:12:51 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello, world!</h1>
+      <p>This is the beginning of the tip calculator app.</p>
+    </>
+  )
+}
+
+export default App
