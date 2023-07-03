2f1b5fcf7da6093de2dfc1af98c7cd9b808ca248
Zowie Erickson <zowie.erickson@gmail.com>
2023-07-03 11:14:18 -0400

    cleanup, remove unused imports in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Bill from './components/Bill'
-import SelectTip from './components/SelectTip'
-import NumberOfPeople from './components/NumberOfPeople'
-import TipAmount from './components/TipAmount'
-import Total from './components/Total'
 import './App.css'
 import DataWrapper from './components/DataWrapper'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="calculator-wrapper">
