872d0af0176e0f27e9122efc6f792f4e9ab46fa6
Zowie Erickson <zowie.erickson@gmail.com>
2023-07-04 10:26:26 -0400

    adjust background color, add logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
+import Logo from './assets/images/logo.svg'
 import DataWrapper from './components/DataWrapper'
 
 function App() {
 
   return (
     <div className="calculator-wrapper">
-      <h1>Tip Calculator app</h1>
+      {/* <h1>Tip Calculator app</h1> */}
+      <img className="logo" src={Logo} alt="Splitter app logo" />
       <DataWrapper />
     </div>
   )
