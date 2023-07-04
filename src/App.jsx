4dc874fcd1585ffd00b9a7ef7cefcf4b8c5e1fa3
Zowie Erickson <zowie.erickson@gmail.com>
2023-07-04 10:50:54 -0400

    reset all margins and padding to 0, split calculation section and totals section to be equal widths

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ function App() {
 
   return (
     <div className="calculator-wrapper">
-      {/* <h1>Tip Calculator app</h1> */}
       <img className="logo" src={Logo} alt="Splitter app logo" />
       <DataWrapper />
     </div>
