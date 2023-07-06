1c5f766f2d7e9d2921028ef325aadb6554055028
Zowie Erickson <zowie.erickson@gmail.com>
2023-07-06 10:57:35 -0400

    update reset button to be initially disabled, but once changes are done to the bill, tip percentage, or number of people, then the button will be clickable to reset the app

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,14 @@
 import '../styles/button.css'
 
-function Button({buttonClass, onClick, children }) {
+function Button({buttonClass, onClick, children, disabled=true }) {
     return (
-        <button className={buttonClass} onClick={onClick}>
+        <button className={buttonClass} onClick={onClick} disabled={disabled}>
             {children}
         </button>
     )
 }
 
-export default function ResetButton({ onStateChangeBill, onStateChangePeopleCount, onStateChangeTipPercentage, onStateChangeCustomTip }) {
+export default function ResetButton({ bill, peopleCount, tipPercentage, customTip, onStateChangeBill, onStateChangePeopleCount, onStateChangeTipPercentage, onStateChangeCustomTip, disabled }) {
     function handleOnClickReset() {
         onStateChangeBill('')
         onStateChangePeopleCount(1)
@@ -16,8 +16,12 @@ export default function ResetButton({ onStateChangeBill, onStateChangePeopleCoun
         onStateChangeCustomTip('')
     }
 
+    if (bill !== '' || tipPercentage !== 0  || tipPercentage !== 0) {
+        disabled = false
+    }
+
     return (
-        <Button buttonClass="reset" onClick={handleOnClickReset}>
+        <Button buttonClass="reset" onClick={handleOnClickReset} disabled={disabled}>
             Reset
         </Button>
     )
